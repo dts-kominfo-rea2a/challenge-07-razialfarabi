343567b5026358c664dc902109e72992e1a9c7ad
Razi Alfarabi <alfarabirazi@gmail.com>
2022-06-11 01:54:32 +0700

    Tugas Promise - Razi Alfarabi

--- promise.js
+++ promise.js
@@ -1,8 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
 
+  let dataHasil = 0;
+
+  const dataIXX = await promiseTheaterIXX();
+  const dataVGC = await promiseTheaterVGC();
+
+  let semuaData = [...dataIXX, ...dataVGC];
+
+  semuaData.forEach(element => {
+    if (element.hasil === emosi) {
+      dataHasil++;
+    }
+  });
+  return dataHasil;
+};
+
+// promiseOutput();
 module.exports = {
   promiseOutput,
 };
